0f1e12a3d393006769866c55b101101f19511583
Ryan Safner <ryansafner@gmail.com>
2021-02-04 11:20:56 -0500

    Update 1.2 content

--- static/slides/make-pdfs.R
+++ static/slides/make-pdfs.R
@@ -1,11 +1,11 @@
 # make-pdfs
-pagedown::chrome_print(input = "https://lawS21.classes.ryansafner.com/slides/1.1-slides.html",
-                       output = "static/slides/1.1-slides.pdf",
+pagedown::chrome_print(input = "https://lawS21.classes.ryansafner.com/slides/1.2-slides.html",
+                       output = "static/slides/1.2-slides.pdf",
                        timeout = 300)
 
 # create png of first slide
-pagedown::chrome_print(input = "https://lawS21.classes.ryansafner.com/slides/1.1-slides.html",
-                       output = "static/slides/1.1-slides.png",
+pagedown::chrome_print(input = "https://lawS21.classes.ryansafner.com/slides/1.2-slides.html",
+                       output = "static/slides/1.2-slides.png",
                        selector = "div.remark-slide",
                        format = "png",
                        timeout = 300)
