75475b9dfcdb916794cd7716ea08e7801ef30a94
Ryan Safner <ryansafner@gmail.com>
2021-05-11 23:20:36 -0400

    HW 3 answers

--- static/slides/make-pdfs.R
+++ static/slides/make-pdfs.R
@@ -1,11 +1,11 @@
 # make-pdfs
-pagedown::chrome_print(input = "https://lawS21.classes.ryansafner.com/slides/4.5-slides.html",
-                       output = "static/slides/4.5-slides.pdf",
+pagedown::chrome_print(input = "https://lawS21.classes.ryansafner.com/slides/5.1-slides.html",
+                       output = "static/slides/5.1-slides.pdf",
                        timeout = 300)
 
 # create png of first slide
-pagedown::chrome_print(input = "https://lawS21.classes.ryansafner.com/slides/4.5-slides.html",
-                       output = "static/slides/4.5-slides.png",
+pagedown::chrome_print(input = "https://lawS21.classes.ryansafner.com/slides/5.1-slides.html",
+                       output = "static/slides/5.1-slides.png",
                        selector = "div.remark-slide",
                        format = "png",
                        timeout = 300)
